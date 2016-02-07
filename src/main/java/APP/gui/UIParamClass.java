04782538075b1e4c52066541220da9869ac46213
wengshengyuan <wengshengyuan@gmail.com>
2016-02-06 22:24:57 -0800

    #

--- src/main/java/APP/gui/UIParamClass.java
+++ src/main/java/APP/gui/UIParamClass.java
@@ -3,44 +3,50 @@ package APP.gui;
 import org.quartz.SchedulerException;
 
 public class UIParamClass {
-	
+
 	private volatile static UIParamClass instance;
-	
+
 	public static UIParamClass getInstance() throws SchedulerException {
-		if(instance == null) {
-			synchronized(UIParamClass.class) {
-				if(instance == null) {
+		if (instance == null) {
+			synchronized (UIParamClass.class) {
+				if (instance == null) {
 					instance = new UIParamClass();
 				}
 			}
 		}
 		return instance;
 	}
-	
-	public static final String STATUS_ERR="未在线";
-	public static final String STATUS_SUCCESS="在线中...";
-	public static final String STATUS_LOGGING="登录中...";
-	
+
+	public static final String STATUS_ERR = "未在线";
+	public static final String STATUS_SUCCESS = "在线中...";
+	public static final String STATUS_LOGGING = "登录中...";
+
 	private String outputLine = "";
 	private String checkStatus = STATUS_ERR;
 	private String lastCheckDate = "";
+
 	public String getCheckStatus() {
 		return checkStatus;
 	}
+
 	public void setCheckStatus(String checkStatus) {
 		this.checkStatus = checkStatus;
 	}
+
 	public String getLastCheckDate() {
 		return lastCheckDate;
 	}
+
 	public void setLastCheckDate(String lastCheckDate) {
 		this.lastCheckDate = lastCheckDate;
 	}
+
 	public String getOutputLine() {
 		return outputLine;
 	}
+
 	public void setOutputLine(String outputLine) {
 		this.outputLine = outputLine;
 	}
-	
+
 }
